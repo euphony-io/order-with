1afe3008c9d499bfc0e45f0974af70b74a1e3a26
phj0407 <89901283+phj0407@users.noreply.github.com>
2022-07-25 20:00:28 +0900

    Update Menu.kt

--- app/src/main/java/com/euphonyio/orderwith/data/dto/Menu.kt
+++ app/src/main/java/com/euphonyio/orderwith/data/dto/Menu.kt
@@ -3,6 +3,7 @@ package com.euphonyio.orderwith.data.dto
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.net.URL
 
 /**
  *  메뉴 Dto
@@ -13,7 +14,7 @@ data class Menu(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "name") val name: String?,
     @ColumnInfo(name = "description") val description: String?,
-    @ColumnInfo(name = "image") val image: String?,
+    @ColumnInfo(name = "image") val image: URL?,
     @ColumnInfo(name = "cost") val cost: Int?
 )
 
