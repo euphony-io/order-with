a23f7b04cc8c02b511b8446c3d56ae87432fdfe0
phj0407 <89901283+phj0407@users.noreply.github.com>
2022-07-26 23:46:12 +0900

    Update Menu.kt
    Delete image Column

--- app/src/main/java/com/euphonyio/orderwith/data/dto/Menu.kt
+++ app/src/main/java/com/euphonyio/orderwith/data/dto/Menu.kt
@@ -14,7 +14,6 @@ data class Menu(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "name") val name: String?,
     @ColumnInfo(name = "description") val description: String?,
-    @ColumnInfo(name = "image") val image: URL?,
     @ColumnInfo(name = "cost") val cost: Int?
 )
 
