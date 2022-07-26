6041e756bd54cba344007f7a9377ded9382a860b
Park Hyejin <hyejin2371@gmail.com>
2022-07-27 01:28:59 +0900

    [Feature] storeMain UI

--- app/src/main/java/com/euphonyio/orderwith/data/dto/OrderMenuItem.kt
+++
@@ -1,7 +0,0 @@
-package com.euphonyio.orderwith.data.dto
-
-/**
- *  주문에 해당되는 메뉴 정보
- *  @author phj0407
- */
-data class OrderMenuItem(var id : Int, var orderId: Int, var menuName : String, var count : Int)
