e15c6ce1544f5fb82b49751316c93681e01a3df2
phj0407 <89901283+phj0407@users.noreply.github.com>
2022-07-27 00:06:31 +0900

    Update OrderMenuDao.kt
    Modify grammar error

--- app/src/main/java/com/euphonyio/orderwith/data/dao/OrderMenuDao.kt
+++ app/src/main/java/com/euphonyio/orderwith/data/dao/OrderMenuDao.kt
@@ -13,17 +13,17 @@ import com.euphonyio.orderwith.data.dto.OrderMenu
  */
 @Dao
 interface OrderMenuDao {
-    @Query("SELCT * FROM OrderMenu")
+    @Query("SELECT * FROM OrderMenu")
     fun getAll(): List<OrderMenu>
 
-    @Query("SELCT om.id as id, om.order_id as orderId, menu.name as menuName, om.count as count from OrderMenu om Left join menu on om.menu_id = menu.id")
+    @Query("SELECT om.id as id, om.order_id as orderId, menu.name as menuName, om.count as count from OrderMenu om Left join menu on om.menu_id = menu.id")
     fun getAllWithMenu(): List<OrderMenuItem>
 
-    @Query("SELCT om.id as id, om.order_id as orderId, menu.name as menuName, om.count as count from OrderMenu om Left join menu on om.menu_id = menu.id " +
+    @Query("SELECT om.id as id, om.order_id as orderId, menu.name as menuName, om.count as count from OrderMenu om Left join menu on om.menu_id = menu.id " +
             "WHERE om.order_id = (:orderId)")
     fun getAllByOrderId(orderId: Int): List<OrderMenuItem>
 
-    @Query("SELCT id FROM OrderMenu order by id desc limit 1")
+    @Query("SELECT id FROM OrderMenu order by id desc limit 1")
     fun getLastId() : Int
 
     @Insert
